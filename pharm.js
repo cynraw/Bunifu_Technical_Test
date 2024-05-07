6beae283199fb9ff94f8635f4ed62e287dfa96ed
Hassan Shakur <dev.hassanshakur@gmail.com>
2024-05-07 19:59:54 +0300

    extra filtering logic

--- pharm.js
+++ pharm.js
@@ -4,26 +4,62 @@ const searchInput = document.querySelector('#search');
 const pharmacyList = [
   {
     name: 'Starlight Pharmacy',
-    address: '123 Main St',
-    town: 'Nairobi',
+    address: '123 Main St Nairobi',
     bgColor: '#e1dcf3',
   },
   {
     name: 'Green Cross Pharmacy',
-    address: '456 Main St',
-    town: 'Nairobi',
+    address: '456 Main St Eldoret',
     bgColor: '#f3e1e1',
   },
   {
     name: 'Sunshine Pharmacy',
-    address: '789 Main St',
-    town: 'Nairobi',
+    address: '789 Main St Nakuru',
     bgColor: '#e1f3e1',
   },
   {
     name: 'Moonlight Pharmacy',
-    address: '101 Main St',
-    town: 'Nairobi',
+    address: '101 Main St Mombasa',
+    bgColor: '#f3f3e1',
+  },
+  {
+    name: 'Some Pharmacy',
+    address: '101 Main St Kitale',
+    bgColor: '#e1dcf3',
+  },
+  {
+    name: 'Another Pharmacy',
+    address: '101 Main St Kisumu',
+    bgColor: '#f3e1e1',
+  },
+  {
+    name: 'Pharmacy 1',
+    address: '101 Main St Thika',
+    bgColor: '#e1f3e1',
+  },
+  {
+    name: 'Pharmacy 2',
+    address: '101 Main St Nyeri',
+    bgColor: '#f3f3e1',
+  },
+  {
+    name: 'Pharmacy 3',
+    address: '101 Main St Meru',
+    bgColor: '#e1dcf3',
+  },
+  {
+    name: 'Pharmacy 4',
+    address: '101 Main St Kericho',
+    bgColor: '#f3e1e1',
+  },
+  {
+    name: 'Pharmacy 5',
+    address: '101 Main St Naivasha',
+    bgColor: '#e1f3e1',
+  },
+  {
+    name: 'Pharmacy 6',
+    address: '101 Main St Nanyuki',
     bgColor: '#f3f3e1',
   },
 ];
@@ -34,7 +70,7 @@ const mappedPharms = pharmacyList.map((pharmacy) => {
   li.classList.add('pharmacy');
   li.innerHTML = `
     <h4>${pharmacy.name}</h4>
-    <p>${pharmacy.address}, ${pharmacy.town}</p>
+    <p>${pharmacy.address}</p>
   `;
   return li;
 });
@@ -49,7 +85,6 @@ searchInput.addEventListener('input', (e) => {
   const filteredPharms = pharmacyList.filter((pharmacy) => {
     return (
       pharmacy.name.toLowerCase().includes(searchValue) ||
-      pharmacy.town.toLowerCase().includes(searchValue) ||
       pharmacy.address.toLowerCase().includes(searchValue)
     );
   });
@@ -60,7 +95,7 @@ searchInput.addEventListener('input', (e) => {
     li.classList.add('pharmacy');
     li.innerHTML = `
         <h4>${pharmacy.name}</h4>
-        <p>${pharmacy.address}, ${pharmacy.town}</p>
+        <p>${pharmacy.address}</p>
         `;
     return li;
   });
